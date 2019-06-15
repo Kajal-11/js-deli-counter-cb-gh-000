47cbc5972be1f1d576cd88d2b97ab835d89d8b3d
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 12:51:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
 function takeANumber(currentCustomer, newCustomer){
-  var array = [];
-  for(let i=0 ; i < newCustomer.length; i++){
-  array.push(`Welcome, ${newCustomer[i]}. You are number ${currentCustomer.length + 1} in line.`);
-  currentCustomer.push(newCustomer[i]);
-  }
-  return array;
+  currentCustomer.push(newCustomer[]);
+  return `Welcome, ${newCustomer}. You are number ${currentCustomer.length} in line.`;
 }
