8f46d749244acaeb02f5acd6b94ab7d07ac96231
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 12:59:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,12 +13,13 @@ function nowServing(currentCustomer){
 }
 
 function currentLine(line){
+  var j;
   if(line.length === 0){
     return "The line is currently empty."
   }
   else{
     var i = "The line is currently:"
-    for(let j=1; j < line.length ; j++){
+    for(j=1; j < line.length ; j++){
       i = i + " " + j + ". " + line[j-1] + ",";
     }
     i = i + " " + j + ". " + line[j-1];
