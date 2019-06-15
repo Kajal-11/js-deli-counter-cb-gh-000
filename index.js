26ed713f003a9b7978306b6cbd713a7953cc67b7
Kajal Goel <kajal11122000@gmail.com>
2019-06-15 12:53:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,12 @@ function takeANumber(currentCustomer, newCustomer){
   currentCustomer.push(newCustomer);
   return `Welcome, ${newCustomer}. You are number ${currentCustomer.length} in line.`;
 }
+
+function nowServing(currentCustomer){
+  if(currentCustomer.length === 0){
+    return "There is nobody waiting to be served!";
+  }
+  else{
+    return currentCustomer.shift();
+  }
+}
